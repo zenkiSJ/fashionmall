0360da8934f9774a42b56c4f3c5a620268dd2b59
zenkiSJ <zenkizerosj@hotmail.com>
2020-12-11 16:46:26 +0100

    add asset and tabbar components

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,61 @@
+<template>
+	<div class="tab-bar-item" @click="itemClick">
+		<div v-if="!isActive" class="icon_img"><slot name="iconImg"></slot></div>
+		<div v-else class="icon_img"><slot name="iconImg_active"></slot></div>
+		<!-- <div :class="{icon_text: isActive}"><slot name="iconText"></slot> </div> -->
+		<div :style="activeStyle "><slot name="iconText"></slot> </div>
+	</div>
+	
+</template>
+
+<script>
+export default {
+	name: "TabBarItem",
+	props: {
+		path: String,
+		activeColor: {
+			type: String,
+			default: 'red'
+		} 
+	},
+	data() {
+		return {
+		}
+	},
+	computed: {
+		isActive(){
+			return this.$route.path.indexOf(this.path) !== -1
+		},
+		activeStyle() {
+			return this.isActive? {color: this.activeColor} : {}
+		}
+	},
+	methods: {
+		itemClick() {
+			this.$router.push(this.path)		
+		}
+	},
+}
+</script>
+
+<style scoped>
+	.tab-bar-item{
+		flex: 1;
+		text-align: center;
+		height: 49px;
+		font-size: 16px;
+		
+	}
+	.icon_img{
+		width: 26px;
+		height: 26px;
+		margin-top: 2px;
+		margin-bottom: 2px;
+		vertical-align: middle;
+	}
+
+	.icon_text{
+		color: red;
+	}	
+	
+</style>
---
+++ src/components/content/MainTabBar.vue
@@ -0,0 +1,37 @@
+
+<template>
+	<div>
+		<tab-bar>
+		<tab-bar-item path="/home" activeColor="blue">
+			<template v-slot:iconImg>
+				<img src="@/assets/images/tabbar/home.png" alt="">
+			</template>
+			<template v-slot:iconImg_active>
+				<img src="@/assets/images/tabbar/home_active.png" alt="">
+			</template>
+			<template v-slot:iconText>
+				<div>首页</div>
+			</template>			
+		</tab-bar-item>
+		<tab-bar-item path="/about">
+			<template v-slot:iconText>
+				<div>关于</div>
+			</template>			
+		</tab-bar-item>
+	</tab-bar>	
+	</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TabBar from '../common/tabbar/TabBar'
+import TabBarItem from '../common/tabbar/TabBarItem.vue'
+
+export default {
+	name: "MainTabBar",
+	components: {
+		TabBar,
+		TabBarItem
+	}		
+}
+</script>
